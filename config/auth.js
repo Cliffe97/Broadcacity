1487c725f749c56b02e4b3f5f99c724f0b63eda0
Cliffe97 <34017648+Cliffe97@users.noreply.github.com>
2018-06-26 14:34:15 -0400

    111

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '797080081212-7h439bjv9r05bljl8fu5qfrbv228h001.apps.googleusercontent.com',
         'clientSecret'  : '7sH4DnuCa_NFIpt620tJpuzr',
-        'callbackURL'   : 'http://127.0.0.1:6200/login/authorized'
+        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6200/login/authorized'
     }
 
 };
