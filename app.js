3053601f10799ec6c130e5ae1de4ea266c49db93
Cliffe97 <34017648+Cliffe97@users.noreply.github.com>
2018-06-26 22:15:00 -0400

    0

--- app.js
+++ app.js
@@ -68,7 +68,6 @@ app.use((req,res,next) => {
     console.log("user has been Authenticated")
     res.locals.user = req.user
     res.locals.loggedIn = true
-    res.redirect('/post');
     if (req.user){
       if (req.user.googleemail=='tjhickey@brandeis.edu'){
         console.log("Owner has logged in")
